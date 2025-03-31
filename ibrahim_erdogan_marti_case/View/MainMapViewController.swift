6b5e7ebbbf8c0b39d0ee767363cd57b9c6961e69
ibrerdogan <94644110+ibrerdogan@users.noreply.github.com>
2025-03-31 22:07:02 +0300

    added base map view

--- ibrahim_erdogan_marti_case/View/MainMapViewController.swift
+++ ibrahim_erdogan_marti_case/View/MainMapViewController.swift
@@ -6,8 +6,17 @@
 //
 
 import UIKit
+import MapKit
 final class MainMapViewController: UIViewController {
     var viewModel: MainMapViewModel
+    
+    
+    private lazy var mainMap: MKMapView = {
+       let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        mapView.showsUserLocation = true
+        return mapView
+    }()
     
     init(viewModel: MainMapViewModel) {
         self.viewModel = viewModel
@@ -16,5 +25,29 @@ final class MainMapViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addComponents()
+    }
+    
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        configureLayout()
+    }
+    
+    private func addComponents() {
+        view.addSubview(mainMap)
+    }
+    
+    private func configureLayout() {
+        NSLayoutConstraint.activate([
+            mainMap.topAnchor.constraint(equalTo: view.topAnchor),
+            mainMap.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mainMap.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mainMap.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 }

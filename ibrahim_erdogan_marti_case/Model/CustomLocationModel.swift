23fb45bffe376ce046092a91654bc7dd42de5f4c
ibrerdogan <94644110+ibrerdogan@users.noreply.github.com>
2025-04-01 22:35:41 +0300

    added custom location model

---
+++ ibrahim_erdogan_marti_case/Model/CustomLocationModel.swift
@@ -0,0 +1,12 @@
+//
+//  CustomLocationModel.swift
+//  ibrahim_erdogan_marti_case
+//
+//  Created by İbrahim Erdogan on 1.04.2025.
+//
+
+import CoreLocation
+struct CustomLocationModel {
+    let address: String
+    let location: CLLocation
+}
